61343b7a23a3432fdfb1f131b1a2f9d91b8e3c67
Benoit Goudreault-Emond <bgoudreaultemond@gmail.com>
2019-01-14 21:05:26 -0500

    French menu font.

--- general/tables/wincharset.asm
+++ general/tables/wincharset.asm
@@ -10,3 +10,18 @@
 	charset $27, $54
 	charset	',', $55
 	charset	'·', $56
+
+	charset 'é', 88
+	charset 'ê', 89
+	charset 'è', 90
+	charset 'ë', 91
+	charset 'â', 92
+	charset 'à', 93
+	charset 'î', 94
+	charset 'ï', 95
+	charset 'ô', 96
+	charset 'û', 97
+	charset 'ù', 98
+	charset 'ü', 99
+	charset 'ç', 100
+
