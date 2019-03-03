26e2e9797f9cae036155f8a83e2b8a796d5e4d6b
Benoit Goudreault-Emond <bgoudreaultemond@gmail.com>
2019-03-02 23:18:16 -0500

    Fix 38. Recompress 12 and 13.

--- script/dialogue 38.asm
+++ script/dialogue 38.asm
@@ -5,21 +5,19 @@
 	dc.b	$98, $01
 	dc.b	"Bienvenue au Temple Gumbious."
 	dc.b	$FD
-	dc.b	"Même les Parmaniens sont les"
-	dc.b	$FC
-	dc.b	"bienvenus."
+	dc.b	"Même les Parmaniens sont les bienvenus."
 	dc.b	$FF
 
 ; $1
-	dc.b	"J'aimerai vous souhaiter la"
+	dc.b	"J'aimerai vous souhaiter la bienvenue,"
 	dc.b	$FC
-	dc.b	"bienvenue, mais le Flambeau..."
+	dc.b	"mais avec le Flambeau qui est..."
 	dc.b	$FF
 
 ; $2
-	dc.b	"Ah, vous avez récupéré le"
+	dc.b	"Ah, vous avez récupéré le Flambeau"
 	dc.b	$FC
-	dc.b	"Flambeau de l'Éclipse!"
+	dc.b	"de l'Éclipse !"
 	dc.b	$FF
 
 ; $3
@@ -27,17 +25,15 @@
 	dc.b	$9B, $02
 	dc.b	$FA
 	dc.b	$98, $01
-	dc.b	"On ne voit pas beaucoup de"
+	dc.b	"On ne voit pas beaucoup de Parmaniens"
 	dc.b	$FC
-	dc.b	"Parmaniens ici. Vous faites du"
-	dc.b	$FD
-	dc.b	"tourisme?"
+	dc.b	"ici. Vous faites du tourisme ?"
 	dc.b	$FF
 
 ; $4
-	dc.b	"Ah, qu'allons-nous faire?"
+	dc.b	"Ah, qu'allons-nous faire ?"
 	dc.b	$FC
-	dc.b	"Le Flambeau de l'Éclipse a..."
+	dc.b	"Le Flambeau de l'Éclipse est..."
 	dc.b	$FF
 
 ; $5
@@ -45,17 +41,15 @@
 	dc.b	$FF
 
 ; $6
-	dc.b	"Ceci est un endroit sacré pour"
+	dc.b	"Ceci est un endroit sacré pour les"
 	dc.b	$FC
-	dc.b	"les Dezolisiens."
+	dc.b	"Dezolisiens."
 	dc.b	$FD
-	dc.b	"Habituellement beaucoup de gens"
+	dc.b	"Habituellement beaucoup de gens le"
 	dc.b	$FC
-	dc.b	"le visitent, mais avec cette"
+	dc.b	"visitent, mais avec cette tempête,"
 	dc.b	$FD
-	dc.b	"tempête, peu de gens peuvent"
-	dc.b	$FC
-	dc.b	"y venir."
+	dc.b	"peu de gens peuvent y venir."
 	dc.b	$FF
 
 ; $7
@@ -65,25 +59,23 @@
 	dc.b	$98, $01
 	dc.b	"Notre flamme sacrée est vraiment"
 	dc.b	$FC
-	dc.b	"magnifique!"
+	dc.b	"magnifique !"
 	dc.b	$FF
 
 ; $8
-	dc.b	"Ah, qu'allons-nous faire?"
+	dc.b	"Ah, qu'allons-nous faire ?"
 	dc.b	$FF
 
 ; $9
-	dc.b	"Je me sens en sécurité,"
+	dc.b	"Je me sens en sécurité, maintenant."
 	dc.b	$FC
-	dc.b	"maintenant. Je vous remercie."
+	dc.b	"Je vous remercie."
 	dc.b	$FF
 
 ; $A
-	dc.b	"On dit qu'une tour appelée la"
+	dc.b	"On dit qu'une tour appelée la « Tour"
 	dc.b	$FC
-	dc.b	"«Tour Corona» était érigée ici"
-	dc.b	$FD
-	dc.b	"jadis."
+	dc.b	"Corona » était érigée ici, jadis."
 	dc.b	$FF
 
 ; $B
@@ -91,19 +83,15 @@
 	dc.b	$9B, $02
 	dc.b	$FA
 	dc.b	$98, $01
-	dc.b	"Le Temple Gumbious n'est pas"
+	dc.b	"Le Temple Gumbious n'est pas uniquement"
 	dc.b	$FC
-	dc.b	"uniquement l'endroit où l'on"
+	dc.b	"l'endroit où l'on préserve le Flambeau"
 	dc.b	$FD
-	dc.b	"préserve le Flambeau de"
+	dc.b	"de l'Éclipse, symbole des croyances"
 	dc.b	$FC
-	dc.b	"l'Éclipse, le symbole des"
+	dc.b	"Dezolisiennes. Nous sommes aussi le"
 	dc.b	$FD
-	dc.b	"croyances Dezolisiennes. Nous"
-	dc.b	$FC
-	dc.b	"sommes aussi le siège"
-	dc.b	$FD
-	dc.b	"du gouvernement."
+	dc.b	"siège du gouvernement."
 	dc.b	$FF
 
 ; $C
@@ -115,13 +103,13 @@
 	dc.b	$FF
 
 ; $D
-	dc.b	"Vous voulez aller récupérer"
+	dc.b	"Vous voulez aller récupérer notre"
 	dc.b	$FC
-	dc.b	"notre flamme sacrée?"
+	dc.b	"flamme sacrée ?"
 	dc.b	$FD
-	dc.b	"Prenez garde de toujours la"
+	dc.b	"Prenez garde de toujours la manipuler"
 	dc.b	$FC
-	dc.b	"manipuler avec respect."
+	dc.b	"avec le respect nécessaire."
 	dc.b	$FF
 
 ; $E
@@ -129,33 +117,31 @@
 	dc.b	$9B, $02
 	dc.b	$FA
 	dc.b	$98, $01
-	dc.b	"Le Flambeau de l'Éclipse est"
+	dc.b	"Le Flambeau de l'Éclipse est notre"
 	dc.b	$FC
-	dc.b	"notre flamme sacrée."
+	dc.b	"flamme sacrée."
 	dc.b	$FD
-	dc.b	"Selon la légende, elle emet une"
+	dc.b	"Selon la légende, elle emet une lumière"
 	dc.b	$FC
-	dc.b	"lumière qui brûle toute chose"
+	dc.b	"qui brûle en cendres toute chose indigne"
 	dc.b	$FD
-	dc.b	"indigne de notre monde en"
-	dc.b	$FC
-	dc.b	"cendres."
+	dc.b	"d'exister dans notre monde."
 	dc.b	$FF
 
 ; $F
-	dc.b	"Quelle horreur!"
+	dc.b	"Quelle horreur !"
 	dc.b	$FC
-	dc.b	"Notre flamme sacrée a été volée!"
+	dc.b	"Notre flamme sacrée a été volée !"
 	dc.b	$FF
 
 ; $10
-	dc.b	"Mon cher Parmanien, si vraiment"
+	dc.b	"Mon cher Parmanien, si vraiment tu as"
 	dc.b	$FC
-	dc.b	"tu as promis de récupérer notre"
+	dc.b	"promis de récupérer notre trésor, aie"
 	dc.b	$FD
-	dc.b	"trésor, veuillez ne pas déroger"
+	dc.b	"l'obligeance de ne pas déroger à cette"
 	dc.b	$FC
-	dc.b	"à cette promesse."
+	dc.b	"promesse."
 	dc.b	$FF
 
 ; $11
@@ -163,35 +149,31 @@
 	dc.b	$9B, $02
 	dc.b	$FA
 	dc.b	$98, $01
-	dc.b	"«Un jour, la flamme du Flambeau"
+	dc.b	"« Un jour, la flamme du Flambeau de"
 	dc.b	$FC
-	dc.b	"de l'Éclipse réduira en cendres"
+	dc.b	"l'Éclipse réduira en cendres tout ce"
 	dc.b	$FD
-	dc.b	"tout ce qui est impur.»"
+	dc.b	"qui est impur. »"
 	dc.b	$FD
-	dc.b	"C'est la légende du Flambeau de"
-	dc.b	$FC
-	dc.b	"l'Éclipse."
+	dc.b	"C'est la légende du Flambeau de l'Éclipse."
 	dc.b	$FF
 
 ; $12
-	dc.b	"«Un jour, la flamme du Flambeau"
+	dc.b	"« Un jour, la flamme du Flambeau de"
 	dc.b	$FC
-	dc.b	"de l'Éclipse réduira en cendres"
+	dc.b	"l'Éclipse réduira en cendres tout ce"
 	dc.b	$FD
-	dc.b	"tout ce qui est impur.»"
+	dc.b	"qui est impur. »"
 	dc.b	$FD
-	dc.b	"Je me demande si la légende dit"
-	dc.b	$FC
-	dc.b	"vrai?"
+	dc.b	"Je me demande si la légende dit vrai ?"
 	dc.b	$FF
 
 ; $13
-	dc.b	"«Un jour, la flamme du Flambeau"
+	dc.b	"« Un jour, la flamme du Flambeau de"
 	dc.b	$FC
-	dc.b	"de l'Éclipse réduira en cendres"
+	dc.b	"l'Éclipse réduira en cendres tout ce"
 	dc.b	$FD
-	dc.b	"tout ce qui est impur.»"
+	dc.b	"qui est impur. »"
 	dc.b	$FD
 	dc.b	"Ce jour est peut-être arrivé."
 	dc.b	$FF
@@ -201,45 +183,35 @@
 	dc.b	$9B, $02
 	dc.b	$FA
 	dc.b	$98, $01
-	dc.b	"J'ai un jour subi de terribles"
-	dc.b	$FC
-	dc.b	"engelures!"
+	dc.b	"J'ai un jour subi de terribles engelures !"
 	dc.b	$FD
-	dc.b	"Mais la lumière du Flambeau de"
+	dc.b	"Mais la lumière du Flambeau de l'Éclipse"
 	dc.b	$FC
-	dc.b	"l'Éclipse m'a guérie en quelques"
+	dc.b	"m'a guérie en quelques instants !"
 	dc.b	$FD
-	dc.b	"instants! C'est vraiment une"
-	dc.b	$FC
-	dc.b	"flamme divine."
+	dc.b	"C'est vraiment une flamme divine."
 	dc.b	$FF
 
 ; $15
-	dc.b	"La lumière ne doit pas être"
-	dc.b	$FC
-	dc.b	"perdue."
+	dc.b	"La lumière ne doit pas être perdue."
 	dc.b	$FD
 	dc.b	"Je vous en prie, cher Parmanien,"
 	dc.b	$FC
-	dc.b	"ramenez notre flamme sacrée!"
+	dc.b	"ramenez notre flamme sacrée !"
 	dc.b	$FF
 
 ; $16
-	dc.b	"Je suis certain que nous pouvons"
+	dc.b	"Je suis certain que nous pouvons faire"
 	dc.b	$FC
-	dc.b	"faire confiance à des gens tels"
+	dc.b	"confiance à des gens tels que vous pour"
 	dc.b	$FD
-	dc.b	"que vous pour prendre soin de"
-	dc.b	$FC
-	dc.b	"la flamme sacrée."
+	dc.b	"bien vous occuper de la flamme sacrée."
 	dc.b	$FF
 
 ; $17
-	dc.b	"Vous ne pouvez pas dépasser"
-	dc.b	$FC
-	dc.b	"cette ligne."
+	dc.b	"Vous ne pouvez pas dépasser cette ligne."
 	dc.b	$FD
-	dc.b	"Veuillez rebrousser le chemin."
+	dc.b	"Veuillez rebrousser votre chemin."
 	dc.b	$FF
 
 ; $18
@@ -247,13 +219,11 @@
 	dc.b	$9B, $02
 	dc.b	$FA
 	dc.b	$98, $01
-	dc.b	"Je suis si heureux."
-	dc.b	$FD
-	dc.b	"C'est si merveilleux de pouvoir"
+	dc.b	"Je suis si heureux. C'est si merveilleux"
 	dc.b	$FC
-	dc.b	"adorer la flamme du Flambeau de"
+	dc.b	"de pouvoir adorer la flamme du Flambeau"
 	dc.b	$FD
-	dc.b	"l'Éclipse de nouveau!"
+	dc.b	"de l'Éclipse de nouveau !"
 	dc.b	$FF
 
 ; $19
@@ -261,11 +231,11 @@
 	dc.b	$FF
 
 ; $1A
-	dc.b	"Ah, vous avez récupéré le"
+	dc.b	"Ah, vous avez récupéré le Flambeau de"
 	dc.b	$FC
-	dc.b	"Flambeau de l'Éclipse."
+	dc.b	"l'Éclipse."
 	dc.b	$FD
-	dc.b	"C'est merveilleux!"
+	dc.b	"C'est merveilleux !"
 	dc.b	$FF
 
 ; $1B
@@ -273,21 +243,17 @@
 	dc.b	$9B, $02
 	dc.b	$FA
 	dc.b	$98, $01
-	dc.b	"Sa lumière me réconforte de plus"
+	dc.b	"Sa lumière me réconforte de plus en plus"
 	dc.b	$FC
-	dc.b	"en plus quand je la regarde."
+	dc.b	"à mesure que je la regarde."
 	dc.b	$FD
-	dc.b	"On dirait que mon coeur est"
-	dc.b	$FC
-	dc.b	"purifié!"
+	dc.b	"On dirait que mon coeur est purifié!"
 	dc.b	$FF
 
 ; $1C
-	dc.b	"Quel...quel outrage d'avoir"
+	dc.b	"Quel... quel outrage d'avoir volé le"
 	dc.b	$FC
-	dc.b	"volé le Flambeau de l'Éclipse"
-	dc.b	$FD
-	dc.b	"si librement!"
+	dc.b	"Flambeau de l'Éclipse si librement !"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
@@ -297,11 +263,9 @@
 	dc.b	$FF
 
 ; $1D
-	dc.b	"Dépêchez-vous de faire ce que"
+	dc.b	"Dépêchez-vous de faire ce que vous"
 	dc.b	$FC
-	dc.b	"vous avez à faire et de le"
-	dc.b	$FD
-	dc.b	"ramener!"
+	dc.b	"devez faire et de nous le ramener !"
 	dc.b	$FF
 
 ; $1E
@@ -309,19 +273,17 @@
 	dc.b	$9B, $02
 	dc.b	$FA
 	dc.b	$98, $01
-	dc.b	"Même les joyaux les plus"
+	dc.b	"Même les joyaux les plus précieux"
 	dc.b	$FC
-	dc.b	"précieux n'émettent une lumière"
-	dc.b	$FD
-	dc.b	"aussi divine."
+	dc.b	"n'émettent une lumière aussi divine."
 	dc.b	$FF
 
 ; $1F
-	dc.b	"Le trésor de tous les"
+	dc.b	"Le trésor de tous les Dezolisiens a été"
 	dc.b	$FC
-	dc.b	"Dezolisien a été dérobé!"
+	dc.b	"dérobé !"
 	dc.b	$FD
-	dc.b	"Que pouvons-nous faire?"
+	dc.b	"Qu'allons-nous faire ?"
 	dc.b	$FF
 
 ; $20
@@ -335,17 +297,17 @@
 	dc.b	$98, $01
 	dc.b	"Je suis un moine qualifié."
 	dc.b	$FD
-	dc.b	"Chaque fois que j'entre ici,"
+	dc.b	"Chaque fois que j'entre ici, je me sens"
 	dc.b	$FC
-	dc.b	"je me sens plus fort."
+	dc.b	"plus fort."
 	dc.b	$FF
 
 ; $22
-	dc.b	"Comment une telle chose a pu"
+	dc.b	"Comment une telle chose a-t-elle pu se"
 	dc.b	$FC
-	dc.b	"arriver? Est-ce à cause de mon"
+	dc.b	"produire ? Est-ce à cause de mon manque"
 	dc.b	$FD
-	dc.b	"manque d'attention?"
+	dc.b	"d'attention ?"
 	dc.b	$FF
 
 ; $23
@@ -353,7 +315,7 @@
 	dc.b	$FD
 	dc.b	"Je peux maintenant reprendre mon"
 	dc.b	$FC
-	dc.b	"entraînement l'esprit en paix."
+	dc.b	"entraînement, l'esprit en paix."
 	dc.b	$FF
 
 ; $24
@@ -363,17 +325,17 @@
 	dc.b	$97, $11
 	dc.b	$F4
 	dc.b	$1A
-	dc.b	"Bienvenue à vous!"
+	dc.b	"Bienvenue à vous !"
 	dc.b	$FD
-	dc.b	"Tous les visteurs sont les"
+	dc.b	"Tous les visteurs sont les bienvenues"
 	dc.b	$FC
-	dc.b	"bienvenues au temple."
+	dc.b	"au temple."
 	dc.b	$FF
 
 ; $25
 	dc.b	$F4
 	dc.b	$1A
-	dc.b	"Je vous souhaite le meilleur!"
+	dc.b	"Je vous souhaite le meilleur !"
 	dc.b	$FF
 
 ; $26
@@ -430,9 +392,7 @@
 	dc.b	$F3
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"Alors voilà le Flambeau de"
-	dc.b	$FC
-	dc.b	"l'Éclipse?"
+	dc.b	"Alors, voici le Flambeau de l'Éclipse ?"
 	dc.b	$FF
 ;; Local Variables:
 ;; eval: (fci-mode)
