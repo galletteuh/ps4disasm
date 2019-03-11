3982beb9130552716461bed1a2773caccda7db5c
galletteuh <galletteuh@gmail.com>
2019-03-11 22:37:09 +0100

    Reformatage dialogue 19.asm

--- script/dialogue 19.asm
+++ script/dialogue 19.asm
@@ -1,101 +1,95 @@
 ; 0
-	dc.b	"Finalement, nous pouvons pousser"
+	dc.b	"Finalement, nous pouvons pousser un"
 	dc.b	$FC
-	dc.b	"un soupir de soulagement! Nous"
+	dc.b	"soupir de soulagement! Nous redonnerons"
 	dc.b	$FD
-	dc.b	"redonnerons à Reshel sa"
+	dc.b	"à Reshel sa splendeur d'origine !"
 	dc.b	$FC
-	dc.b	"splendeur d'origine! Non,"
-	dc.b	$FD
-	dc.b	"plutôt, elle sera encore mieux!"
+	dc.b	"Non, plutôt, elle sera encore mieux !"
 	dc.b	$FF
 
 ; $1
-	dc.b	"J'ai entendu dire que"
+	dc.b	"J'ai entendu dire que l'armurerie est déjà"
 	dc.b	$FC
-	dc.b	"l'armurerie est déjà rouverte."
+	dc.b	"rouverte."
 	dc.b	$FD
-	dc.b	"Je ferais mieux d'y faire un"
-	dc.b	$FC
-	dc.b	"tour!"
+	dc.b	"Je ferais mieux d'y faire un tour !"
 	dc.b	$FF
 
 ; $2
-	dc.b	"Il y a beaucoup de souvenirs"
+	dc.b	"Il y a beaucoup de souvenirs ici, bons"
 	dc.b	$FC
-	dc.b	"ici, bons et mauvais..."
+	dc.b	"et mauvais..."
 	dc.b	$FD
-	dc.b	"Mais c'est dur de quitter sa"
+	dc.b	"Mais c'est dur de quitter sa ville"
 	dc.b	$FC
-	dc.b	"ville natale."
+	dc.b	"natale."
 	dc.b	$FF
 
 ; $3
-	dc.b	"Vous êtes certains que tout ira"
+	dc.b	"Vous êtes certains que tout ira bien"
 	dc.b	$FC
-	dc.b	"bien maintenant?"
+	dc.b	"maintenant ?"
 	dc.b	$FD
-	dc.b	"J'espère qu'il n'y aura plus de"
+	dc.b	"J'espère qu'il n'y aura plus de zombis"
 	dc.b	$FC
-	dc.b	"zombis sortant de toute part!"
+	dc.b	"sortant de toute part !"
 	dc.b	$FF
 
 ; $4
-	dc.b	"Vous verrez! Les gens qui ont"
+	dc.b	"Vous verrez ! Les gens qui ont abandonné"
 	dc.b	$FC
-	dc.b	"abandonné la ville reviendront"
+	dc.b	"la ville reviendront si on travaille dur."
 	dc.b	$FD
-	dc.b	"si on travaille dur."
+	dc.b	""
 	dc.b	$FF
 
 ; $5
-	dc.b	"Si je veux attirer des clients"
+	dc.b	"Si je veux attirer des clients dans ma"
 	dc.b	$FC
-	dc.b	"dans ma boutique nouvellement"
+	dc.b	"boutique nouvellement rénovée,"
 	dc.b	$FD
-	dc.b	"rénovée, il vaudrait mieux avoir"
+	dc.b	"il vaudrait mieux avoir des soldes"
 	dc.b	$FC
-	dc.b	"des soldes compétitives."
+	dc.b	"compétitives."
 	dc.b	$FF
 
 ; $6
-	dc.b	"Papa est parti chez le voisin"
+	dc.b	"Papa est parti chez le voisin donner un"
 	dc.b	$FC
-	dc.b	"donner un coup de main."
+	dc.b	"coup de main."
 	dc.b	$FD
-	dc.b	"Il m'a demandé d'être sage"
+	dc.b	"Il m'a demandé d'être sage et d'attendre"
 	dc.b	$FC
-	dc.b	"et d'attendre qu'il revienne."
+	dc.b	"qu'il revienne."
 	dc.b	$FF
 
 ; $7
 	dc.b	"Vous êtes ici à Reshel."
 	dc.b	$FC
-	dc.b	"Bienvenue à mes frères"
-	dc.b	$FD
-	dc.b	"Parmaniens!"
+	dc.b	"Bienvenue à mes frères Parmaniens !"
 	dc.b	$FF
 
 ; $8
-	dc.b	"Ma boutique d'outils est aussi"
+	dc.b	"Ma boutique d'outils est aussi rénovée"
 	dc.b	$FC
-	dc.b	"rénovée récemment."
+	dc.b	"récemment."
 	dc.b	$FD
-	dc.b	"Venez-donc y faire un tour!"
+	dc.b	"Venez-donc y faire un tour !"
 	dc.b	$FF
 
 ; $9
-	dc.b	"Nous remplirons la nouvelle"
+	dc.b	"Nous remplirons la nouvelle ville de"
 	dc.b	$FC
-	dc.b	"ville de Reshel de mémoires"
+	dc.b	"Reshel de mémoires heureuses !"
 	dc.b	$FD
-	dc.b	"heureuses! Vous verrez!"
+	dc.b	"Vous verrez !"
 	dc.b	$FF
 
 ; $A
-	dc.b	"On dirait que nous sommes"
+	dc.b	"On dirait que nous sommes maintenant"
 	dc.b	$FC
-	dc.b	"maintenant libres de problèmes!"
+	dc.b	"libres de problèmes !"
 	dc.b	$FF
 
 ; $B
@@ -105,261 +99,233 @@
 	dc.b	$FF
 
 ; $C
-	dc.b	"C'est fantastique! La ville de"
+	dc.b	"C'est fantastique ! La ville de Reshel"
 	dc.b	$FC
-	dc.b	"Reshel était en ruine. Voyez à"
+	dc.b	"était en ruine. Voyez à quelle vitesse"
 	dc.b	$FD
-	dc.b	"quelle vitesse elle se relève!"
+	dc.b	"elle se relève !"
 	dc.b	$FF
 
 ; $D
 	dc.b	"Je viens d'emménager ici..."
 	dc.b	$FD
-	dc.b	"Est-ce qu'il s'est passé"
+	dc.b	"Est-ce qu'il s'est passé d'étranges"
 	dc.b	$FC
-	dc.b	"d'étranges choses récemment?"
+	dc.b	"choses récemment ?"
 	dc.b	$FF
 
 ; $E
-	dc.b	"Tout est comme cela doit être:"
-	dc.b	$FD
-	dc.b	"le climat, ainsi que l'apparence"
+	dc.b	"Tout est comme cela doit être : le climat,"
 	dc.b	$FC
-	dc.b	"de notre ville."
+	dc.b	"ainsi que l'apparence de notre ville."
 	dc.b	$FF
 
 ; $F
-	dc.b	"Finalement! Les affaires"
+	dc.b	"Finalement ! Les affaires semblent"
 	dc.b	$FC
-	dc.b	"semblent vouloir reprendre."
+	dc.b	"vouloir reprendre."
 	dc.b	$FF
 
 ; $10
-	dc.b	"Papa a fini d'aider les voisins,"
+	dc.b	"Papa a fini d'aider les voisins, mais là,"
 	dc.b	$FC
-	dc.b	"mais là, il ne fait que traîner."
+	dc.b	"il ne fait que traîner."
 	dc.b	$FD
-	dc.b	"Il m'a dit de rester tranquille"
+	dc.b	"Il m'a dit de rester tranquille et de faire"
 	dc.b	$FC
-	dc.b	"et de faire ce qu'on me dit."
+	dc.b	"ce qu'on me dit."
 	dc.b	$FF
 
 ; $11
-	dc.b	"Hé, toi! Tu salis tout!"
+	dc.b	"Hé, toi ! Tu salis tout !"
 	dc.b	$FC
-	dc.b	"Tes souliers sont plein de boue!"
+	dc.b	"Tes souliers sont plein de boue !"
 	dc.b	$FD
-	dc.b	"Je viens juste de rénover tout"
+	dc.b	"Je viens juste de rénover tout ici,"
 	dc.b	$FC
-	dc.b	"ici, j'aimerais garder ça"
-	dc.b	$FD
-	dc.b	"propre!"
+	dc.b	"j'aimerais garder ça propre !"
 	dc.b	$FF
 
 ; $12
-	dc.b	"Je suis très heureuse"
+	dc.b	"Je suis très heureuse d'avoir une maison"
 	dc.b	$FC
-	dc.b	"d'avoir une maison toute neuve!"
+	dc.b	"toute neuve !"
 	dc.b	$FD
-	dc.b	"Il faut que j'utilise mon"
+	dc.b	"Il faut que j'utilise mon insecticide pour"
 	dc.b	$FC
-	dc.b	"insecticide pour pas que les"
-	dc.b	$FD
-	dc.b	"insectes nous envahissent!"
+	dc.b	"pas que les insectes nous envahissent !"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"Argh! Aspergez les insectes, pas"
-	dc.b	$FC
-	dc.b	"moi!"
+	dc.b	"Argh ! Aspergez les insectes, pas moi !"
 	dc.b	$FF
 
 ; $13
-	dc.b	"Zut, je venais juste de me faire"
+	dc.b	"Zut, je venais juste de me faire des"
 	dc.b	$FC
-	dc.b	"des nouveaux amis où on avait"
-	dc.b	$FD
-	dc.b	"déménagé..."
+	dc.b	"nouveaux amis où on avait déménagé..."
 	dc.b	$FF
 
 ; $14
-	dc.b	"Oh là là. Je suis presque"
+	dc.b	"Oh là là. Je suis presque ruiné à cause de"
 	dc.b	$FC
-	dc.b	"ruiné à cause de tous ces"
+	dc.b	"tous ces déménagements. Mais l'avenir"
 	dc.b	$FD
-	dc.b	"déménagements. Mais l'avenir"
+	dc.b	"est prometteur !"
+	dc.b	$FD
+	dc.b	"Tu verras ! Si c'est l'argent qui compte,"
 	dc.b	$FC
-	dc.b	"est prometteur!"
-	dc.b	$FD
-	dc.b	"Tu verras! Si c'est l'argent qui"
-	dc.b	$FC
-	dc.b	"compte, j'en aurai autant que"
-	dc.b	$FD
-	dc.b	"je voudrai!"
+	dc.b	"j'en aurai autant que je voudrai !"
 	dc.b	$FF
 
 ; $15
-	dc.b	"C'est surprenant de l'entendre"
+	dc.b	"C'est surprenant de l'entendre parler"
 	dc.b	$FC
-	dc.b	"parler ainsi."
+	dc.b	"ainsi."
 	dc.b	$FD
-	dc.b	"Je pensais qu'il n'était qu'un"
+	dc.b	"Je pensais qu'il n'était qu'un guignol, mais"
 	dc.b	$FC
-	dc.b	"guignol, mais j'ai changé"
-	dc.b	$FD
-	dc.b	"d'idée à son sujet!"
+	dc.b	"j'ai changé d'idée à son sujet !"
 	dc.b	$FF
 
 ; $16
 	dc.b	$FA
 	dc.b	$A1, $01
 	dc.b	"Ici, c'est la ville de Meese."
-	dc.b	$FD
-	dc.b	"Nous sommes en difficulté à"
 	dc.b	$FC
-	dc.b	"cause d'une étrange maladie qui"
+	dc.b	"Nous sommes en difficulté à cause d'une"
 	dc.b	$FD
-	dc.b	"se propage dans la ville."
+	dc.b	"étrange maladie qui se propage dans la"
+	dc.b	$FC
+	dc.b	"ville."
 	dc.b	$FF
 
 ; $17
 	dc.b	"Ici, c'est la ville de Meese."
 	dc.b	$FD
-	dc.b	"Les gens malades commencent à"
+	dc.b	"Les gens malades commencent à se"
 	dc.b	$FC
-	dc.b	"se rétablir."
+	dc.b	"rétablir."
 	dc.b	$FF
 
 ; $18
 	dc.b	$FA
 	dc.b	$A1, $01
-	dc.b	"Il y a une maladie très bizarre"
+	dc.b	"Il y a une maladie très bizarre qui se"
 	dc.b	$FC
-	dc.b	"qui se propage en ce moment..."
+	dc.b	"propage en ce moment..."
 	dc.b	$FD
-	dc.b	"Les malades font beaucoup de"
+	dc.b	"Les malades font beaucoup de fièvre et"
 	dc.b	$FC
-	dc.b	"fièvre et meurent en un mois."
+	dc.b	"meurent en un mois."
 	dc.b	$FD
-	dc.b	"En plus, les morts ressuscitent,"
+	dc.b	"En plus, les morts ressuscitent, mais"
 	dc.b	$FC
-	dc.b	"mais deviennent des zombis!"
+	dc.b	"deviennent des zombis !"
 	dc.b	$FD
-	dc.b	"C'est vraiment terrifiant!"
+	dc.b	"C'est vraiment terrifiant !"
 	dc.b	$FF
 
 ; $19
-	dc.b	"Tout-à-coup, les malades ont"
+	dc.b	"Tout-à-coup, les malades ont cessé de"
 	dc.b	$FC
-	dc.b	"cessé de faire de la fièvre."
+	dc.b	"faire de la fièvre."
 	dc.b	$FF
 
 ; $1A
 	dc.b	$FA
 	dc.b	$A1, $01
-	dc.b	"Il y avait une ville, Reshel,"
+	dc.b	"Il y avait une ville, Reshel, à l'ouest..."
 	dc.b	$FC
-	dc.b	"à l'ouest..."
+	dc.b	"Il semble qu'elle ait été décimée par une"
 	dc.b	$FD
-	dc.b	"Il semble qu'elle ait été"
-	dc.b	$FC
-	dc.b	"décimée par une maladie!"
-	dc.b	$FD
-	dc.b	"On dit que c'est une ville"
+	dc.b	"maladie ! On dit que c'est une ville"
 	dc.b	$FC
 	dc.b	"morte-vivante depuis."
 	dc.b	$FD
-	dc.b	"Il faut prendre ça au sérieux"
-	dc.b	$FC
-	dc.b	"ici!"
+	dc.b	"Il faut prendre ça au sérieux ici !"
 	dc.b	$FF
 
 ; $1B
-	dc.b	"Il semble que les zombis ont"
+	dc.b	"Il semble que les zombis ont disparus, et"
 	dc.b	$FC
-	dc.b	"disparus, et Reshel se"
-	dc.b	$FD
-	dc.b	"reconstruit peu à peu!"
+	dc.b	"Reshel se reconstruit peu à peu !"
 	dc.b	$FF
 
 ; $1C
 	dc.b	$FA
 	dc.b	$A1, $01
-	dc.b	"Certains ont fuit Reshel, mais"
+	dc.b	"Certains ont fuit Reshel, mais nous ne"
 	dc.b	$FC
-	dc.b	"nous ne sommes pas nécessaire-"
+	dc.b	"sommes pas nécessairement plus en"
 	dc.b	$FD
-	dc.b	"ment en sécurité ici!"
+	dc.b	"sécurité ici !"
 	dc.b	$FF
 
 ; $1D
-	dc.b	"Ceux qui avaient fuit Reshel"
+	dc.b	"Ceux qui avaient fuit Reshel sont"
 	dc.b	$FC
-	dc.b	"sont retournés chez eux."
+	dc.b	"retournés chez eux."
 	dc.b	$FD
-	dc.b	"Ils ont dit qu'ils allaient tout"
+	dc.b	"Ils ont dit qu'ils allaient tout reprendre"
 	dc.b	$FC
-	dc.b	"reprendre à zéro."
+	dc.b	"à zéro."
 	dc.b	$FF
 
 ; $1E
 	dc.b	$FA
 	dc.b	$A1, $01
-	dc.b	"L...l...les zombies ont peur"
+	dc.b	"L...l...les zombies ont peur du feu,"
 	dc.b	$FC
-	dc.b	"du feu, pas vrai?"
+	dc.b	"pas vrai ?"
 	dc.b	$FD
-	dc.b	"Donc, ils ne viendront pas nous"
+	dc.b	"Donc, ils ne viendront pas nous attaquer"
 	dc.b	$FC
-	dc.b	"attaquer si on fait un feu au"
+	dc.b	"si on fait un feu au milieu de la ville,"
 	dc.b	$FD
-	dc.b	"milieu de la ville, pas vrai?"
+	dc.b	"pas vrai ?"
 	dc.b	$FF
 
 ; $1F
-	dc.b	"Il n'y aura plus de zombis,"
+	dc.b	"Il n'y aura plus de zombis, pas vrai ?"
 	dc.b	$FC
-	dc.b	"pas vrai?"
-	dc.b	$FD
-	dc.b	"Alors éteignons ces feux! Vite!"
+	dc.b	"Alors éteignons ces feux ! Vite !"
 	dc.b	$FF
 
 ; $20
-	dc.b	"Ici, c'est le triage des"
+	dc.b	"Ici, c'est le triage des patients."
 	dc.b	$FC
-	dc.b	"patients. C'était une auberge"
+	dc.b	"C'était une auberge avant, mais"
 	dc.b	$FD
-	dc.b	"avant, mais l'aubergiste en a"
+	dc.b	"l'aubergiste en a fait une clinique"
 	dc.b	$FC
-	dc.b	"fait une clinique ouverte à"
-	dc.b	$FD
-	dc.b	"tous par bonté de coeur."
+	dc.b	"ouverte à tous par bonté de coeur."
 	dc.b	$FF
 
 ; $21
 	dc.b	$FA
 	dc.b	$A1, $01
-	dc.b	"Plus de la moitié des habitants"
+	dc.b	"Plus de la moitié des habitants de cette"
 	dc.b	$FC
-	dc.b	"de cette ville sont malades."
+	dc.b	"ville sont malades."
 	dc.b	$FF
 
 ; $22
-	dc.b	"Je suis si content que tout le"
+	dc.b	"Je suis si content que tout le monde se"
 	dc.b	$FC
-	dc.b	"monde se porte mieux."
+	dc.b	"porte mieux."
 	dc.b	$FF
 
 ; $23
-	dc.b	"Vous avez vu le cimetière?"
+	dc.b	"Vous avez vu le cimetière ?"
 	dc.b	$FD
-	dc.b	"C'est vraiment horrible, mais"
+	dc.b	"C'est vraiment horrible, mais nous avons"
 	dc.b	$FC
-	dc.b	"nous avons dû brûler et enterrer"
+	dc.b	"dû brûler et enterrer les dépouilles de"
 	dc.b	$FD
-	dc.b	"les dépouilles de ceux qui sont"
+	dc.b	"ceux qui sont devenus des Zombis."
 	dc.b	$FC
-	dc.b	"devenus des Zombis."
+	dc.b	""
 	dc.b	$FF
 
 ; $24
@@ -367,28 +333,20 @@
 	dc.b	$A1, $02
 	dc.b	$FA
 	dc.b	$95, $01
-	dc.b	"Un «Esper» est venu ici? C'est"
-	dc.b	$FC
-	dc.b	"louche!"
+	dc.b	"Un « Esper » est venu ici ? C'est louche !"
 	dc.b	$FF
 
 ; $25
-	dc.b	"Un «Esper» est venu ici? C'est"
-	dc.b	$FC
-	dc.b	"louche!"
-	dc.b	$FD
-	dc.b	" "
+	dc.b	"Un « Esper » est venu ici ? C'est louche !"
 	dc.b	$F4
 	dc.b	$0A
-	dc.b	"Comment oses-tu dire ça!"
+	dc.b	"Comment oses-tu dire ça !"
 	dc.b	$FF
 
 ; $26
-	dc.b	"Les malades se rétablissent."
+	dc.b	"Les malades se rétablissent. Je me"
 	dc.b	$FC
-	dc.b	"Je me demande si c'est grâce à"
-	dc.b	$FD
-	dc.b	"ces «Espers»?"
+	dc.b	"demande si c'est grâce à ces « Espers » ?"
 	dc.b	$FF
 
 ; $27
@@ -396,45 +354,43 @@
 	dc.b	$A1, $02
 	dc.b	$FA
 	dc.b	$95, $01
-	dc.b	"Le propriétaire de cette"
+	dc.b	"Le propriétaire de cette boutique"
 	dc.b	$FC
-	dc.b	"boutique d'outils a été terrassé"
+	dc.b	"d'outils a été terrassé par la maladie et"
 	dc.b	$FD
-	dc.b	"par la maladie et a dû fermer."
+	dc.b	"a dû fermer."
 	dc.b	$FD
-	dc.b	"Il vendait une sorte de parfum"
+	dc.b	"Il vendait une sorte de parfum dont le"
 	dc.b	$FC
-	dc.b	"dont le nom m'échappe."
+	dc.b	"nom m'échappe."
 	dc.b	$FF
 
 ; $28
-	dc.b	"Le propriétaire de cette"
+	dc.b	"Le propriétaire de cette boutique"
 	dc.b	$FC
-	dc.b	"boutique d'outils a été terrassé"
+	dc.b	"d'outils a été terrassé par la maladie et"
 	dc.b	$FD
-	dc.b	"par la maladie et a dû fermer."
+	dc.b	"a dû fermer."
 	dc.b	$FD
-	dc.b	"Il vendait une sorte de parfum"
+	dc.b	"Il vendait une sorte de parfum dont le"
 	dc.b	$FC
-	dc.b	"dont le nom m'échappe."
+	dc.b	"nom m'échappe."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$0A
-	dc.b	"Oh, du parfum! C'est chouette."
+	dc.b	"Oh, du parfum ! C'est chouette."
 	dc.b	$FF
 
 ; $29
-	dc.b	"Je suis heureuse que la boutique"
+	dc.b	"Je suis heureuse que la boutique d'outils"
 	dc.b	$FC
-	dc.b	"d'outils soit de nouveau"
+	dc.b	"soit de nouveau ouverte, mais ces"
 	dc.b	$FD
-	dc.b	"ouverte, mais ces parfums"
-	dc.b	$FC
-	dc.b	"coûtent vraiment cher!"
+	dc.b	"parfums coûtent vraiment cher !"
 	dc.b	$FF
 
 ; $2A
-	dc.b	"Youpi! Youpi!"
+	dc.b	"Youpi ! Youpi !"
 	dc.b	$FF
 ;; Local Variables:
 ;; eval: (fci-mode)
