fceb9947709e38136ed070f49ac8ef67422b857d
galletteuh <galletteuh@gmail.com>
2019-03-14 21:26:29 +0100

    Reformatage dialogue 22.asm

--- script/dialogue 22.asm
+++ script/dialogue 22.asm
@@ -160,135 +160,119 @@
 ; $35
 	dc.b	$F4
 	dc.b	$08
-	dc.b	"La force gravitationnelle est"
+	dc.b	"La force gravitationnelle est dans les"
 	dc.b	$FC
-	dc.b	"dans les normes, et il y a une"
+	dc.b	"normes, et il y a une atmosphère"
 	dc.b	$FD
-	dc.b	"atmosphère respirable. Il s'agit"
+	dc.b	"respirable. Il s'agit manifestement de"
 	dc.b	$FC
-	dc.b	"manifestement de conditions"
-	dc.b	$FD
-	dc.b	"maintenues artificiellement."
+	dc.b	"conditions maintenues artificiellement."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"On dirait que nous sommes"
+	dc.b	"On dirait que nous sommes attendus..."
 	dc.b	$FC
-	dc.b	"attendus..."
-	dc.b	$FC
-	dc.b	"Hein!"
+	dc.b	"Hein !"
 	dc.b	$FF
 
 ; $36
 	dc.b	$F4
 	dc.b	$1D
-	dc.b	"Ha, ha, ha, ha!"
+	dc.b	"Ha, ha, ha, ha !"
 	dc.b	$FD
-	dc.b	"Vous voilà! Malheureusement..."
+	dc.b	"Vous voilà ! Malheureusement..."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$1F
-	dc.b	"Ha, ha, ha, ha!"
+	dc.b	"Ha, ha, ha, ha !"
 	dc.b	$FD
-	dc.b	"Seul Lutz nous intéresse!"
+	dc.b	"Seul Lutz nous intéresse !"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$1D
-	dc.b	"Et nous n'avons qu'une chose à"
+	dc.b	"Et nous n'avons qu'une chose à lui"
 	dc.b	$FC
-	dc.b	"lui proposer."
+	dc.b	"proposer."
 	dc.b	$FD
-	dc.b	"Ha, ha, ha, ha!"
+	dc.b	"Ha, ha, ha, ha !"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$1F
-	dc.b	"Le mutiler et le tuer de nos"
+	dc.b	"Le mutiler et le tuer de nos propres"
 	dc.b	$FC
-	dc.b	"propres mains!"
+	dc.b	"mains !"
 	dc.b	$FF
 
 ; $37
 	dc.b	$F4
 	dc.b	$1C
-	dc.b	"Alors, tu t'es décidé à"
-	dc.b	$FC
-	dc.b	"venir...Lutz!"
+	dc.b	"Alors, tu t'es décidé à venir... Lutz !"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$04
-	dc.b	"Donc, il s'agissait bien de"
-	dc.b	$FC
-	dc.b	"Lashiec!"
+	dc.b	"Donc, il s'agissait bien de Lashiec !"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$1C
-	dc.b	"Ha ha ha...J'ai tant..."
+	dc.b	"Ha ha ha... J'ai tant..."
 	dc.b	$FC
-	dc.b	"J'ai tant attendu ce moment!"
+	dc.b	"J'ai tant attendu ce moment !"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$04
-	dc.b	"Lashiec! Pourquoi es-tu"
+	dc.b	"Lashiec ! Pourquoi es-tu réincarné..."
 	dc.b	$FC
-	dc.b	"réincarné...en cette chose?"
+	dc.b	"en cette chose ?"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$1C
-	dc.b	"Pour me venger!"
+	dc.b	"Pour me venger !"
 	dc.b	$FD
-	dc.b	"Lutz, toi...et tous tes"
+	dc.b	"Lutz, toi... et tous tes compagnons..."
 	dc.b	$FC
-	dc.b	"compagnons...vont pâtir de"
+	dc.b	"vont pâtir de douleurs éternelles !"
 	dc.b	$FD
-	dc.b	"douleurs éternelles!"
+	dc.b	"Je vais vous tuer, tous !"
 	dc.b	$FD
-	dc.b	"Je vais vous tuer, tous!"
+	dc.b	"Vous deviendrez comme... moi !"
 	dc.b	$FD
-	dc.b	"Vous deviendrez comme... moi!"
-	dc.b	$FD
-	dc.b	"Vous deviendrez vous aussi des"
+	dc.b	"Vous deviendrez vous aussi des âmes"
 	dc.b	$FC
-	dc.b	"âmes damnées qui ne pourront"
+	dc.b	"damnées qui ne pourront jamais quitter"
 	dc.b	$FD
-	dc.b	"jamais quitter ce château!"
+	dc.b	"ce château!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"Alors, c'est pour ça que tu"
+	dc.b	"Alors, c'est pour ça que tu nous a"
 	dc.b	$FC
-	dc.b	"nous a attiré ici?"
+	dc.b	"attiré ici ?"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$1C
-	dc.b	"Ha ha ha...Quelle joie!"
-	dc.b	$FD
-	dc.b	"Vous avez répondu à mon"
+	dc.b	"Ha ha ha...Quelle joie !"
 	dc.b	$FC
-	dc.b	"invitation..."
+	dc.b	"Vous avez répondu à mon invitation..."
 	dc.b	$FD
-	dc.b	"J'ai attendu deux mille ans"
+	dc.b	"J'ai attendu deux mille ans pour avoir"
 	dc.b	$FC
-	dc.b	"pour avoir l'occasion de vous"
-	dc.b	$FD
-	dc.b	"trancher la gorge!"
+	dc.b	"l'occasion de vous trancher la gorge !"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"Il attaque!"
+	dc.b	"Il attaque !"
 	dc.b	$FF
 
 ; $38
 	dc.b	$F4
 	dc.b	$1C
-	dc.b	"Ahhhh! Tu es un démon, Lutz!"
-	dc.b	$FD
-	dc.b	"Tu m'as vaincu, mais tu ne"
+	dc.b	"Ahhhh! Tu es un démon, Lutz ! Tu m'as"
 	dc.b	$FC
-	dc.b	"vaincra jamais..."
+	dc.b	"vaincu, mais tu ne vaincra jamais..."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"Le Flambeau de l'Éclipse?!"
+	dc.b	"Le Flambeau de l'Éclipse ?!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$06
@@ -296,44 +280,40 @@
 	dc.b	$F5
 	dc.b	$F2, $00
 	dc.w	$0108
-	dc.b	"Ah-hah!"
+	dc.b	"Ah-hah !"
 	dc.b	$F9
 	dc.b	$3B
 
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$0A
-	dc.b	"Nous y sommes arrivés!"
+	dc.b	"Nous y sommes arrivés !"
 	dc.b	$FD
-	dc.b	"Nous avons récupéré le Flambeau"
+	dc.b	"Nous avons récupéré le Flambeau de"
 	dc.b	$FC
-	dc.b	"de l'Éclipse!"
+	dc.b	"l'Éclipse !"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$1C
-	dc.b	"Je fonds! Je fonds!!!"
+	dc.b	"Je fonds! Je fonds !!!"
 	dc.b	$FD
 	dc.b	$F2, $03
 	dc.b	$ED
 	dc.b	$F2, $00
 	dc.w	$0109
-	dc.b	"Une fois encore, mon corps ce"
+	dc.b	"Une fois encore, mon corps ce disloque !"
 	dc.b	$FC
-	dc.b	"disloque!"
+	dc.b	"Mais sachez que malgré cela, je ne"
 	dc.b	$FD
-	dc.b	"Mais sachez que malgré cela,"
+	dc.b	"mourrai pas! Je me réincarnerai sans"
 	dc.b	$FC
-	dc.b	"je ne mourrai pas! Je me"
+	dc.b	"cesse."
 	dc.b	$FD
-	dc.b	"réincarnerai sans cesse."
-	dc.b	$FD
-	dc.b	"Je dois accomplir Sa volonté!"
-	dc.b	$FD
-	dc.b	"Tant qu'Il vit, je demeurerai"
+	dc.b	"Je dois accomplir Sa volonté ! Tant qu'Il"
 	dc.b	$FC
-	dc.b	"et apporterai le malheur dans"
+	dc.b	"vit, je demeurerai et apporterai le"
 	dc.b	$FD
-	dc.b	"tout Algo!"
+	dc.b	"malheur dans tout Algo !"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$00
@@ -379,19 +359,19 @@
 
 	dc.b	$F4
 	dc.b	$08
-	dc.b	"Le Air Castle s'effondre!"
+	dc.b	"Le Air Castle s'effondre !"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"Il faut sortir d'ici, vite!"
+	dc.b	"Il faut sortir d'ici, vite !"
 	dc.b	$FD
 	dc.b	$F7
 
 	dc.b	$F4
 	dc.b	$08
-	dc.b	"Une onde de choc va nous"
+	dc.b	"Une onde de choc va nous atteindre."
 	dc.b	$FC
-	dc.b	"atteindre. Prenez garde!"
+	dc.b	"Prenez garde !"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$04
@@ -399,75 +379,69 @@
 	dc.w	$010B
 	dc.b	"Lashiec..."
 	dc.b	$FD
-	dc.b	"Quand on y pense, son sort est"
+	dc.b	"Quand on y pense, son sort est bien"
 	dc.b	$FC
-	dc.b	"bien triste."
+	dc.b	"triste."
 	dc.b	$FD
 	dc.b	$F2, $00
 	dc.w	$010C
-	dc.b	"Il fut séduit par un grand"
+	dc.b	"Il fut séduit par un grand pouvoir..."
 	dc.b	$FC
-	dc.b	"pouvoir... mais il est devenu"
-	dc.b	$FD
-	dc.b	"un vulgaire pantin!"
+	dc.b	"mais il est devenu un vulgaire pantin !"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"Un grand pouvoir?"
+	dc.b	"Un grand pouvoir ?"
 	dc.b	$FD
-	dc.b	"Qui était ce «Il» dont Lashiec"
-	dc.b	$FC
-	dc.b	"parlait?"
+	dc.b	"Qui était ce « Il » dont Lashiec parlait ?"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$06
-	dc.b	"Était-ce... Dark Force?"
+	dc.b	"Était-ce... Dark Force ?"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"Nous ne l'avions pas détruite?"
+	dc.b	"Nous ne l'avions pas détruite ?"
 	dc.b	$FF
 
 ; $39
 	dc.b	$F4
 	dc.b	$1A
-	dc.b	"Bien! C'est vraiment le"
+	dc.b	"Bien ! C'est vraiment le Flambeau de"
 	dc.b	$FC
-	dc.b	"Flambeau de l'Éclipse!"
+	dc.b	"l'Éclipse !"
 	dc.b	$FD
-	dc.b	"Vous avez accompli tout un"
+	dc.b	"Vous avez accompli tout un exploit en"
 	dc.b	$FC
-	dc.b	"exploit en le retrouvant!"
+	dc.b	"le retrouvant !"
 	dc.b	$FD
-	dc.b	"Je n'ai pas de réserves à prêter"
+	dc.b	"Je n'ai pas de réserves à prêter le"
 	dc.b	$FC
-	dc.b	"le Flambeau à des gens tels"
+	dc.b	"Flambeau à des gens tels que vous."
 	dc.b	$FD
-	dc.b	"que vous."
-	dc.b	$FD
-	dc.b	"Utilisez la flamme sacrée pour"
+	dc.b	"Utilisez la flamme sacrée pour nous"
 	dc.b	$FC
-	dc.b	"nous éloigner du chemin du mal."
+	dc.b	"éloigner du chemin du mal."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"Excellent! Dépêchons-nous vers"
+	dc.b	"Excellent ! Dépêchons-nous vers cette"
 	dc.b	$FC
-	dc.b	"cette forêt d'arbres carnivores!"
+	dc.b	"forêt d'arbres carnivores !"
 	dc.b	$FF
 
 ; $3A
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"Qu'est-ce que c'est que ça?"
+	dc.b	"Qu'est-ce que c'est que ça ?"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$06
-	dc.b	"C'est Dark Force?!"
+	dc.b	"C'est Dark Force ?!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$04
-	dc.b	"C'est elle, j'en suis sûr!"
+	dc.b	"C'est elle, j'en suis sûr !"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$08
@@ -475,43 +449,39 @@
 	dc.b	$FC
 	dc.b	"historiques..."
 	dc.b	$FD
-	dc.b	"Ceci n'est pas comparable à"
+	dc.b	"Ceci n'est pas comparable à quelque"
 	dc.b	$FC
-	dc.b	"quelque objet situé sur Kuran."
+	dc.b	"objet situé sur Kuran."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$06
-	dc.b	"Alors il y avait deux Dark"
-	dc.b	$FC
-	dc.b	"Forces?"
+	dc.b	"Alors il y avait deux Dark Forces ?"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"C'est tel que Raja l'avait dit!"
+	dc.b	"C'est tel que Raja l'avait dit ! Voici ce qui"
 	dc.b	$FC
-	dc.b	"Voici ce qui cause tous ces"
-	dc.b	$FD
-	dc.b	"ravages sur Dezolis!"
+	dc.b	"cause tous ces ravages sur Dezolis !"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$04
 	dc.b	"Pas seulement sur Dezolis."
 	dc.b	$FD
-	dc.b	"Elle fiche la pagaille dans tout"
+	dc.b	"Elle fiche la pagaille dans tout le système"
 	dc.b	$FC
-	dc.b	"le système solaire d'Algo!"
+	dc.b	"solaire d'Algo !"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$08
-	dc.b	"Dark Force perçoit notre"
+	dc.b	"Dark Force perçoit notre précence."
 	dc.b	$FC
-	dc.b	"précence. Elle va nous attaquer!"
+	dc.b	"Elle va nous attaquer !"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"C'est ce qu'on espérait!"
+	dc.b	"C'est ce qu'on espérait !"
 	dc.b	$FC
-	dc.b	"Tout le monde, à l'assaut!"
+	dc.b	"Tout le monde, à l'assaut !"
 	dc.b	$FF
 
 ; $3B
@@ -525,13 +495,13 @@
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$06
-	dc.b	"La tempête...elle s'estompe!"
+	dc.b	"La tempête... elle s'estompe !"
 	dc.b	$FD
 	dc.b	$F7
 
 	dc.b	$F4
 	dc.b	$06
-	dc.b	"Regardez! Le ciel!"
+	dc.b	"Regardez ! Le ciel !"
 	dc.b	$F9
 	dc.b	$77
 
@@ -547,13 +517,11 @@
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$04
-	dc.b	"L'Onde d'Énergie Noire s'est"
+	dc.b	"L'Onde d'Énergie Noire s'est dissipée."
 	dc.b	$FC
-	dc.b	"dissipée. Je parie que les"
+	dc.b	"Je parie que les malades de Meese se"
 	dc.b	$FD
-	dc.b	"malades de Meese se portent"
-	dc.b	$FC
-	dc.b	"déjà mieux."
+	dc.b	"portent déjà mieux."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$0A
@@ -569,69 +537,61 @@
 	dc.w	$0112
 	dc.b	"Merci beaucoup."
 	dc.b	$FD
-	dc.b	"C'est grâce à vous tous que nous"
+	dc.b	"C'est grâce à vous tous que nous avons"
 	dc.b	$FC
-	dc.b	"avons accompi cela."
+	dc.b	"accompi cela."
 	dc.b	$FD
-	dc.b	"J'ai été plutôt prétentieuse de"
+	dc.b	"J'ai été plutôt prétentieuse de croire"
 	dc.b	$FC
-	dc.b	"croire que je pouvais y arriver"
-	dc.b	$FD
-	dc.b	"seule!"
+	dc.b	"que je pouvais y arriver seule !"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$06
-	dc.b	"C'est sans importance. Je suis"
+	dc.b	"C'est sans importance. Je suis heureuse"
 	dc.b	$FC
-	dc.b	"heureuse pour toi, Kyra!"
+	dc.b	"pour toi, Kyra !"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$0A
-	dc.b	"Bon! Je dois retourner à Meese."
-	dc.b	$FD
-	dc.b	"J'ai beaucoup de pain sur la"
+	dc.b	"Bon ! Je dois retourner à Meese."
 	dc.b	$FC
-	dc.b	"planche!"
+	dc.b	"J'ai beaucoup de pain sur la planche !"
 	dc.b	$FD
-	dc.b	"Au revoir, Chaz. Tu es un vrai"
+	dc.b	"Au revoir, Chaz. Tu es un vrai trésor,"
 	dc.b	$FC
-	dc.b	"trésor, je te considère comme"
-	dc.b	$FD
-	dc.b	"mon petit frère!"
+	dc.b	"je te considère comme mon petit frère !"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
 	dc.b	$F2, $00
 	dc.w	$0113
-	dc.b	"Hein! Ne soit pas absurde!"
+	dc.b	"Hein ! Ne soit pas absurde !"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$0A
-	dc.b	"Ha ha ha! C'est pour ça que tu"
+	dc.b	"Ha ha ha ! C'est pour ça que tu es si"
 	dc.b	$FC
-	dc.b	"es si attendrissant."
+	dc.b	"attendrissant."
 	dc.b	$FD
-	dc.b	"À bientôt, tout le monde!"
+	dc.b	"À bientôt, tout le monde !"
 	dc.b	$FC
-	dc.b	"À bientôt, Lutz!"
+	dc.b	"À bientôt, Lutz !"
 	dc.b	$FD
-	dc.b	"Vous n'étiez pas tout à fait"
+	dc.b	"Vous n'étiez pas tout à fait comme je"
 	dc.b	$FC
-	dc.b	"comme je l'imaginais, mais vous"
+	dc.b	"l'imaginais, mais vous avez amplement"
 	dc.b	$FD
-	dc.b	"avez amplement fait honneur"
+	dc.b	"fait honneur à votre nom !"
 	dc.b	$FC
-	dc.b	"à votre nom!"
-	dc.b	$FD
 	dc.b	"Nous nous reverrons sous peu."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$04
-	dc.b	"Hein? Euh, oui, bien sûr."
+	dc.b	"Hein ? Euh, oui, bien sûr."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$0A
-	dc.b	"Au revoir! À bientôt!"
+	dc.b	"Au revoir ! À bientôt !"
 	dc.b	$FD
 	dc.b	$F7
 
@@ -643,86 +603,82 @@
 	dc.b	$01
 	dc.b	"Elle m'a traitée de petit frère."
 	dc.b	$FC
-	dc.b	"Suis-je si peu fiable?"
+	dc.b	"Suis-je si peu fiable ?"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$04
-	dc.b	"Eh bien, tu t'es un peu"
+	dc.b	"Eh bien, tu t'es un peu amélioré,"
 	dc.b	$FC
-	dc.b	"amélioré, mais tu me ressemble"
-	dc.b	$FD
-	dc.b	"encore beaucoup!"
+	dc.b	"mais tu me ressemble encore beaucoup !"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
 	dc.b	$F2, $00
 	dc.w	$0114
-	dc.b	"Bah, je ne suis pas à ta"
+	dc.b	"Bah, je ne suis pas à ta hauteur."
 	dc.b	$FC
-	dc.b	"hauteur. Tu es Lutz, après tout!"
+	dc.b	"Tu es Lutz, après tout !"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$04
 	dc.b	$F2, $00
 	dc.w	$0115
-	dc.b	"Tu veux que je te botte le"
+	dc.b	"Tu veux que je te botte le derrière"
 	dc.b	$FC
-	dc.b	"derrière encore une fois!?"
+	dc.b	"encore une fois !?"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$06
-	dc.b	"Vous n'en avez jamais assez,"
+	dc.b	"Vous n'en avez jamais assez, vous"
 	dc.b	$FC
-	dc.b	"vous deux!?"
+	dc.b	"deux !?"
 	dc.b	$FD
 	dc.b	$F7
 
 	dc.b	$F4
 	dc.b	$06
-	dc.b	"Quoi? C'est le Temple Gumbious!"
+	dc.b	"Quoi ? C'est le Temple Gumbious !"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"Comment est-ce possible? Nous"
+	dc.b	"Comment est-ce possible ? Nous avons"
 	dc.b	$FC
-	dc.b	"avons vaincu Dark Force!"
+	dc.b	"vaincu Dark Force !"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$04
-	dc.b	"Allons voir!"
+	dc.b	"Allons voir !"
 	dc.b	$FF
 
 ; $3C
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"Oh non!"
+	dc.b	"Oh non !"
 	dc.b	$FC
-	dc.b	"Ce flambeau est un faux!"
+	dc.b	"Ce flambeau est un faux !"
 	dc.b	$FF
 
 ; $3D
 	dc.b	$F3
 	dc.b	$F4
 	dc.b	$0A
-	dc.b	"Ce sont les gredins qui ont"
+	dc.b	"Ce sont les gredins qui ont volé le"
 	dc.b	$FC
-	dc.b	"volé le Flambeau de l'Éclipse!"
+	dc.b	"Flambeau de l'Éclipse !"
 	dc.b	$FD
-	dc.b	"Ils sont une illusion?"
+	dc.b	"Ils sont une illusion ?"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$04
-	dc.b	"Ce n'est pas simplement une"
+	dc.b	"Ce n'est pas simplement une illusion..."
 	dc.b	$FC
-	dc.b	"illusion... Je perçois une"
-	dc.b	$FD
-	dc.b	"barrière invisible!"
+	dc.b	"Je perçois une barrière invisible !"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"Alors, il faudra trouver les"
+	dc.b	"Alors, il faudra trouver les vrais et les"
 	dc.b	$FC
-	dc.b	"vrais et les détruire!"
+	dc.b	"détruire !"
 	dc.b	$FF
 ;; Local Variables:
 ;; eval: (fci-mode)
