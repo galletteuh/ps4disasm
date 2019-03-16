a621a37cc5b8290d0659ba000484851eaed21448
Benoit Goudreault-Emond <bgoudreaultemond@gmail.com>
2019-03-15 23:30:51 -0400

    Fix title text (scrolling part).
    Adjusted timing slightly because variable width font helps
    a lot with longer sentences.

--- ps4.constants.asm
+++ ps4.constants.asm
@@ -1898,7 +1898,7 @@ Command_Defense  = 5
 
 
 WindowSize_CombatTransient = $13	; width of "Cannot flee!"/"Surprise Attack!" window
-Title_ScrollDelay = $2E				; number of lines to show for scrolling title text
+Title_ScrollDelay = $2D				; number of lines to show for scrolling title text
 Hunter_guild_cursor_x = $100		; X position of red cursor (pixels) for Hunter's guild window
 
 ; ---------------------------------------------------------------------------
